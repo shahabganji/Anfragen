da27c0eae14e679dad85ec20b5e45f9de6719c85
Saeed Ganji <shahabganji@gmail.com>
2018-11-28 18:22:09 +0330

    // WIP: refactor ( demo )
    Change age validation to prevent throwing exceptions on non-numeric values

--- src/Anfragen.Demo/Program.cs
+++ src/Anfragen.Demo/Program.cs
@@ -4,81 +4,88 @@ using Anfragen.Interfaces;
 using System;
 
 namespace Anfragen.Demo {
-	internal class Program {
-		public static void Main(string[ ] args) {
+    internal class Program {
+        public static void Main( string[ ] args ) {
 
-			UserConsole terminal = new UserConsole( );
-			IQuestionnaire questionnaire = new Questionnaire( terminal );
-			questionnaire.Settings.WelcomeMessage = "Welcome to my questionnare";
+            UserConsole terminal = new UserConsole( );
+            IQuestionnaire questionnaire = new Questionnaire( terminal );
+            questionnaire.Settings.WelcomeMessage = "Welcome to my questionnare";
 
-			Func<Question, bool> validator = x=> x.Answer.Length > 0;
-			string  errorMessage = "Please Provide a value";
+            Func<Question, bool> validator = x => x.Answer.Length > 0;
+            string errorMessage = "Please Provide a value";
 
-			Question ask_name = new Prompt( "What's your name?" ).Validator( validator,errorMessage);
-			Question ask_family = new Prompt( "What's your family?" ).Validator( validator,errorMessage);
+            Question ask_name = new Prompt( "What's your name?" ).Validator( validator, errorMessage );
+            Question ask_family = new Prompt( "What's your family?" ).Validator( validator, errorMessage );
 
-			var builder = new SelectableListQuestionBuilder();
+            var builder = new SelectableListQuestionBuilder( );
 
-			Question preferable_language =	builder
-											.New("What's your favorite language?")
-											.AddValidation(x => x.Answer != null)
-											.AddOption(new QuestionOption("Persian"))
-											.AddOptions(new[] {
-												new QuestionOption("English"),
-												new QuestionOption("Italian"),
-												new QuestionOption("Spanish"),
-												new QuestionOption("French"),
-												new QuestionOption("German")
-											})
-											.AsCheckList()
-											.AddToQuestionnaire(questionnaire)
-											.WithErrorMessage("You must select an option")
-											.Build();
+            Question preferable_language = builder
+                                            .New( "What's your favorite language?" )
+                                            .AddValidation( x => x.Answer != null )
+                                            .AddOption( new QuestionOption( "Persian" ) )
+                                            .AddOptions( new[ ] {
+                                                new QuestionOption("English"),
+                                                new QuestionOption("Italian"),
+                                                new QuestionOption("Spanish"),
+                                                new QuestionOption("French"),
+                                                new QuestionOption("German")
+                                            } )
+                                            .AsCheckList( )
+                                            .AddToQuestionnaire( questionnaire )
+                                            .WithErrorMessage( "You must select an option" )
+                                            .Build( );
 
-			questionnaire
-				.Add(ask_name)
-				.Add(ask_family)
-				;
+            questionnaire
+                .Add( ask_name )
+                .Add( ask_family )
+                ;
 
-			questionnaire.Start();
+            questionnaire.Start( );
 
-			bool add = true;
+            bool add = true;
 
-			// loop until there is a question to ask
-			while (questionnaire.CanProceed) {
+            // loop until there is a question to ask
+            while ( questionnaire.CanProceed ) {
 
-				if (add) {
+                if ( add ) {
 
-					Confirm confirm  = new Confirm("Are you older than 18?");
+                    Confirm confirm = new Confirm( "Are you older than 18?" );
 
-					confirm.Validator(x => {
-						Confirm q = (Confirm)x;
-						return q.PossibleAnswers.Contains(x.Answer);
-					}, "Your value should be either 'Yes' or 'No'");
+                    confirm.Validator( x => {
+                        Confirm q = ( Confirm ) x;
+                        return q.PossibleAnswers.Contains( x.Answer );
+                    }, "Your value should be either 'Yes' or 'No'" );
 
-					questionnaire.Confirm(confirm);
+                    questionnaire.Confirm( confirm );
 
-					Prompt age_prompt = new Prompt("How old are you?");
-					age_prompt.Validator(x => int.Parse(x.Answer) >= 18, "Your must be older than 18");
-					questionnaire.Prompt(age_prompt);
-					add = false;
-				}
+                    Prompt age_prompt = new Prompt( "How old are you?" );
+                    age_prompt.Validator( x => {
 
-				questionnaire.Next();
+                        int age;
+                        int.TryParse( x.Answer, out age );
 
-			}
+                        return age >= 18;
 
-			questionnaire.End();
+                    }, "Your must be older than 18" );
+                    questionnaire.Prompt( age_prompt );
+                    add = false;
+                }
 
-			// Print Processed questions
-			foreach (Question q in questionnaire.ProcessedQuestions) {
+                questionnaire.Next( );
 
-				terminal.Printer.Write($"{q.Text} : {q.Answer}");
-				terminal.Printer.WriteLine();
+            }
 
-			}
+            questionnaire.End( );
 
-			Console.ReadLine();
-		}
-	}
+            // Print Processed questions
+            foreach ( Question q in questionnaire.ProcessedQuestions ) {
+
+                terminal.Printer.Write( $"{q.Text} : {q.Answer}" );
+                terminal.Printer.WriteLine( );
+
+            }
+
+            Console.ReadLine( );
+        }
+    }
 }
