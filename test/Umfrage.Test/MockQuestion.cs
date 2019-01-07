759737c6381ae2686e43642f0e701d64f185acb9
Saeed Ganji <shahabganji@gmail.com>
2019-01-07 14:41:51 +0330

    // WIP: unit_test( Question )
    Writing unit tests for the Question class

--- test/Umfrage.Test/MockQuestion.cs
+++ test/Umfrage.Test/MockQuestion.cs
@@ -5,12 +5,14 @@ namespace Umfrage.Test {
     public class MockQuestion : Question {
 
         public MockQuestion( string question, IQuestionnaire questionnaire = null ) : base( question, questionnaire ) {
-
         }
 
         protected override IQuestion TakeAnswer( ) {
             this.Answer = this.Terminal.Scanner.ReadLine( );
             this.Terminal.Printer.Write( this.Answer );
+
+			this.Validate();
+
             return this;
         }
     }
